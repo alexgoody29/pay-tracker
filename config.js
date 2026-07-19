c3a96e05a46d9c5fd4122f1d5c97e62685e4e652
alexgoody29 <alexgoody29@gmail.com>
2026-07-20 04:08:48 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,19 @@
+const PAY_TRACKER_CONFIG = {
+    employers: {
+        seaworld: {
+            id: "seaworld",
+            name: "SeaWorld",
+            baseRate: 35.00
+        },
 
+        "queensland-health": {
+            id: "queensland-health",
+            name: "Queensland Health",
+            baseRate: 46.98
+        }
+    },
+
+    storageKeys: {
+        shifts: "payTrackerShifts"
+    }
+};
